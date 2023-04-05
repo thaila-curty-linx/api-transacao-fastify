1b2101c44a7259bc2596a34aa6216abfcf78cdfb
thailajf <thailajf@hotmail.com>
2023-04-04 22:25:34 -0300

    start project

---
+++ src/server.ts
@@ -0,0 +1,11 @@
+import fastify from "fastify";
+
+const app = fastify();
+
+app.get("/hello", () => {
+  return "Hello world";
+});
+
+app.listen({ port: 3333 }).then(() => {
+  console.log("http server running");
+});
