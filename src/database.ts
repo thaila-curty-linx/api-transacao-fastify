b2bb11d6a8f7003e7695ebf4a5d067e1b291f674
thailajf <thailajf@hotmail.com>
2023-04-12 22:26:42 -0300

    [master] add envs and error handling with zod

---
+++ src/database.ts
@@ -0,0 +1,13 @@
+import { knex as setupKnex, Knex } from "knex";
+import { env } from "./env";
+if (!process.env.DATABASE_URL) {
+  throw new Error("DATABASE_URL env not found");
+}
+
+export const config: Knex.Config = {
+  client: "sqlite3",
+  connection: { filename: env.DATABASE_URL },
+  useNullAsDefault: true,
+  migrations: { extension: "ts", directory: "./db/migrations" },
+};
+export const knex = setupKnex(config);
--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,15 @@
 import fastify from "fastify";
+import crypto from "node:crypto";
+import { knex } from "./database";
+import { env } from "./env";
 
 const app = fastify();
 
-app.get("/hello", () => {
-  return "Hello world";
+app.get("/hello", async () => {
+  const transactions = await knex("transactions").select("*");
+  return transactions;
 });
 
-app.listen({ port: 3333 }).then(() => {
+app.listen({ port: env.PORT }).then(() => {
   console.log("http server running");
 });
