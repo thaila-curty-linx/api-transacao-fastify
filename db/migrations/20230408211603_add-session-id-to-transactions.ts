7752999e1e3687129c91a9b3e14fec42e10b2547
Thaila <thailajf@hotmail.com>
2023-06-08 00:17:14 -0300

    [master] add dependencies and test script in package.json

--- db/migrations/20230408211603_add-session-id-to-transactions.ts
+++ db/migrations/20230408211603_add-session-id-to-transactions.ts
@@ -2,12 +2,12 @@ import { Knex } from 'knex'
 
 export async function up(knex: Knex): Promise<void> {
   await knex.schema.alterTable('transactions', (table) => {
-    table.uuid('sessionId').after('id').index()
+    table.uuid('session_id').after('id').index()
   })
 }
 
 export async function down(knex: Knex): Promise<void> {
   await knex.schema.alterTable('transactions', (table) => {
-    table.dropColumn('sessionId')
+    table.dropColumn('session_id')
   })
 }
