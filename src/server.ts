048de4d68c9a95e6d240774b7118d798b0697f26
Thaila <thailajf@hotmail.com>
2023-06-08 00:19:20 -0300

    [master] adjust code - divide app file - use .env.test if NODE_ENV is teste

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,5 @@
-import fastify from 'fastify'
+import { app } from './app'
 import { env } from './env'
-import { transactionsRoutes } from './routes/transactions'
-
-const app = fastify()
-
-app.register(transactionsRoutes, { prefix: 'transactions' })
 
 app.listen({ port: env.PORT }).then(() => {
   console.log('http server running')
